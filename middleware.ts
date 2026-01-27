edfc977a1d0a599c4a13950b10d25eaec050806d
Rattanak Hong <code.giwce@aleeas.com>
2026-01-27 18:17:16 +0700

    changed the function name from proxy for middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/proxy";
 
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   // update user's auth session
   return await updateSession(request);
 }
@@ -13,7 +13,6 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
      */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
